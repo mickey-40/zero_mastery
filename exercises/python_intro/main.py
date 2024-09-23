e7579df5c1a2854c8057037cf22b95d883ec78c0
mickey-40 <mickeyarnold53@yahoo.com>
2024-09-22 19:02:07 -0500

    int, float, and math functions

--- exercises/python_intro/main.py
+++ exercises/python_intro/main.py
@@ -1,3 +1,39 @@
-name = input('What is your name?')
+# name = input('What is your name?')
 
-print('hello ' + name)
+# print('hello ' + name)
+
+# Fundamental Data Types
+# int & float 
+# print(4+4)
+# print(2-4)
+# print(type(2*4))
+# print(type(2/4))
+
+# print(2**3)
+# print(5//2)
+# print(5%4)
+
+# Math functions
+print(round(3.1))
+print(round(3.9))
+print(abs(3.1))
+print(abs(-3.1))
+
+
+
+
+
+# bool
+# str
+# list
+# tuple
+# set
+# dict
+
+#Classes -> custom types
+
+# Specialized data types
+
+None
+
+
