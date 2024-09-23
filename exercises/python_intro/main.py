64e306aee3d99dea6d2a466980b118aca47cbfe5
mickey-40 <mickeyarnold53@yahoo.com>
2024-09-23 18:01:08 -0500

    notes from the day off google docs

--- exercises/python_intro/main.py
+++ exercises/python_intro/main.py
@@ -64,6 +64,83 @@ None
 # statements-statement that preforms an action
 
 # augmented assignment operator
-some_value = 5
-some_value += 2
-print(some_value)
+# some_value = 5
+# some_value += 2
+# print(some_value)
+
+# String
+# str - uses ‘ ‘ or “ “ anything can go into the single or double quotes
+# Long_string = ‘’’
+# Wow
+# O O 
+# —
+# ‘’’ 3 single quotes for long strings or multi-line strings
+
+# String concatenation adding strings together
+
+# ‘Hello’ + ‘Mickey’
+
+# Escape Sequences
+
+# Inside a string \ lets python know that the next thing is a string or different functions like tab or go to next line
+
+# Formated strings
+
+# name = ‘Johnny’
+# age = 55
+
+# print(f’hi {name}. You are {age} years old’)
+
+# Python 2
+# name = ‘Johnny’
+# age = 55
+
+# print(’hi {}. You are {} years old’.format(name,age))
+
+# Change order
+
+# print(’hi {1}. You are {0} years old’.format(name,age))
+
+# Create Variables
+
+# print(’hi {0}. You are {1} years old’.format(name=’sally’,age=100))
+
+# String Indexes
+
+# sentence = ‘Me me me’
+
+# Indexes start at 0 so sentence[0] = ‘M’
+
+# String slicing
+# #  [start : stop : stepover]
+
+# So sentence[0:2] = ‘Me’
+
+# So sentence[0 : 8 : 2] = ‘Mmm’
+
+# Sentence[: : -1] = ‘em em eM’
+
+# Strings are Immutabile 
+
+# Type Conversion
+# Example:
+# birth_year = int(input(‘what year were you born?’))
+
+# age = 2024 - birth_year
+
+# print(f‘your age is:{age}’)
+
+# List is a data structure
+
+# The square brackets keeps different data types
+
+# amazon_cart = [ ‘notebook’, ‘sunglasses’]
+
+# print(amazon_cart[0])
+
+# List slicing same as strings
+
+# amazon_cart = [ ‘notebook’, ‘sunglasses’, ‘toys’, ‘grapes’]
+
+# print(amazon_cart)
+
