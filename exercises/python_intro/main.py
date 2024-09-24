77fa0c261e71d67ba9d138966f56b4fe7a2578d9
mickey-40 <mickeyarnold53@yahoo.com>
2024-09-24 18:27:52 -0500

    notes on tuple, sets and methods of set methods

--- exercises/python_intro/main.py
+++ exercises/python_intro/main.py
@@ -153,7 +153,176 @@ None
 
 # print(matrix[0][1])
 
-bastet = ['a', 'b', 'c', 'd', 'e']
+# bastet = ['a', 'b', 'c', 'd', 'e']
 
-print('a' in bastet)
+# print('a' in bastet)
+# List unpacking
+# Example:
+# a,b, c, *other = [1,2,3,4,5,6,7]
+
+# print(a) output = 1
+# print(b) output = 2
+# print(c) output = 3
+# print(other) output = [4,5,6,7]
+
+# a,b, c, *other, d = [1,2,3,4,5,6,7,8]
+
+# print(a) output = 1
+# print(b) output = 2
+# print(c) output = 3
+# print(other) output = [4,5,6,7]
+# print(d) output = 8 last element
+
+# Dictionary (dict) - data type and structure
+
+# # -*- coding: utf-8 -*-
+# """
+# Created on Sun Aug 16 10:35:42 2020
+
+# @author: saura
+# """
+# my_dict = {
+#     'a' : [1,2,3],
+#     'b' : "hello",
+#     'c' : True
+#     }
+
+# my_list = [
+#     {
+#     'a' : [1,2,3],
+#     'b' : "hello",
+#     'c' : True
+#     },
+#     {
+#     'a' : [4,5,6],
+#     'b' : "bye",
+#     'c' : False
+#     }
+#     ]
+
+# print(my_dict["a"])
+# print(my_dict["a"][1])
+# print(my_list[1]["a"][2])
+
+# Dictionary keys have to be immutable 
+
+
+# # -*- coding: utf-8 -*-
+# """
+# Created on Sun Aug 16 11:33:18 2020
+
+# @author: saura
+# """
+# my_tuple = (1,2,3,4,5,2)
+# print(my_tuple[0])
+# print(my_tuple[1:2])    # be carefull here, we also get a 'comma' when we just store a signle tuple value
+# print(my_tuple[0:2])
+# print(my_tuple[::-2])
+# print(2 in my_tuple)
+
+# # my_tuple[0] = 4     # it will be an error, because tuple are immutable
+# print(my_tuple)
+
+# print(my_tuple.count(2))
+# print(my_tuple.index(5))
+
+# my_dict = {
+#     "age": 45,
+#     (1,2): "hello"
+#     }
+# print(my_dict)
+# print(my_dict.items())   # returns key:value pair as a tuple
+# print(my_dict[(1,2)])
+
+# a,b,c,*other = (1,2,3,4,5,6,7,8,9)  # it stores as list if the variable has more than 1 item, otherwise as int.
+# print(a)
+# print(type(a))
+# print(other)
+# print(type(other))
+
+
+# # -*- coding: utf-8 -*-
+# """
+# Created on Sun Aug 16 11:55:22 2020
+
+# @author: saura
+# """
+# my_set = {1,2,3,4,5,5,5}
+# my_set.add(100)
+# my_set.add(2)
+# print(my_set)
+# # print(my_set[0])  # we cannot do this, it produces an error, because set is an unordered collection of objects
+# print(len(my_set))
+
+# print(5 in my_set)
+# print(my_set)
+
+# print(list(my_set))
+
+# new_set = my_set.copy()
+# print(my_set.clear())
+# print(my_set)
+# print(new_set)
+
+# my_list = [1,2,3,4,5,5,5]
+# print(set(my_list))     # this way we can remove the duplicate items from the list
+
+# # -*- coding: utf-8 -*-
+# """
+# Created on Sun Aug 16 12:15:33 2020
+
+# @author: saura
+# """
+# # this is very similar to Venn diagrams.
+
+# my_set = {1,2,3,4,5}
+# your_set = {4,5,6,7,8,9,10}
+
+# # .difference()
+# # .discard()
+# # .difference_update()
+# # .intersection()
+# # .isdisjoint()
+# # .issubset()
+# # .issuperset()
+# # .union()
+
+# print("\ndifference")
+# print(my_set.difference(your_set))
+# print(my_set)
+
+# print("\ndiscard")
+# print(my_set.discard(5))
+# print(my_set)
+
+# print("\ndifference_update")
+# print(my_set.difference_update(your_set))
+# print(my_set)
+
+# my_set = {1,2,3,4,5}
+
+# print("\nintersection")
+# print(my_set.intersection(your_set))
+# print(my_set & your_set)
+
+# print("\nisdisjoint")
+# print(my_set.isdisjoint(your_set))
+# my_set = {1,2,3}
+# print(my_set.isdisjoint(your_set))
+
+# print("\nissubset")
+# print(my_set.issubset(your_set))
+# my_set = {4,6,8}
+# print(my_set.issubset(your_set))
+
+# print("\nissuperset")
+# print(my_set.issuperset(your_set))
+# print(your_set.issuperset(my_set))
+
+# print("\nunion")
+# my_set = {1,2,3,4,5}
+# print(my_set.union(your_set))
+# # or
+# print(my_set | your_set)
+
 
